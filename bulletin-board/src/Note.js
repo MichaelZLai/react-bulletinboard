fa7b0796caad1d3783469574e1d78c8072cf4655
Michael Lai <mike24567@gmail.com>
2019-02-11 22:17:38 -0800

    adding ability to add notes

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -14,6 +14,7 @@ class Note extends Component{
     this.save = this.save.bind(this)
     this.renderForm = this.renderForm.bind(this)
     this.renderDisplay = this.renderDisplay.bind(this)
+    // this.onChange = this.onChange.bind(this)
   }
 
   edit(){
@@ -26,16 +27,20 @@ class Note extends Component{
     alert("removing")
   }
 
-  save(){
-    alert("saved")
+  save(e){
+    e.preventDefault()
+    this.props.onChange(this._newText.value, this.props.index)
+    this.setState({
+      editing: false
+    })
   }
 
   renderForm(){
     return(
       <div className="note">
-        <form>
-          <textarea />
-          <button onClick={this.save}><GiCompactDisc /></button>
+        <form onSubmit={this.save}>
+          <textarea ref={input => this._newText = input}/>
+          <button id="save"><GiCompactDisc /></button>
         </form>
       </div>
     )
@@ -44,7 +49,7 @@ class Note extends Component{
   renderDisplay(){
     return(
       <div className="note">
-        <p>Learn React</p>
+        <p>{this.props.children}</p>
         <span>
           <button onClick={this.edit} id="edit"><FaPencilAlt /></button>
           <button onClick={this.remove} id="remove"><FaTrash /></button>
